e39624e178fb8fbf698e769d40a671778892668f
cyriakleclercq <43751447+cyriakleclercq@users.noreply.github.com>
2019-11-28 16:00:22 +0100

    ajout des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { ALaCarteComponent } from './a-la-carte/a-la-carte.component';
+import { CagnotteComponent } from './cagnotte/cagnotte.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CommandesComponent } from './commandes/commandes.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { LoginComponent } from './login/login.component';
+import { MenuSemaineComponent } from './menu-semaine/menu-semaine.component';
+import { PanierComponent } from './panier/panier.component';
+import { PlatComponent } from './plat/plat.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'nav', component: NavComponent },
+  { path: 'a-la-carte', component: ALaCarteComponent },
+  { path: 'cagnotte', component: CagnotteComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'commandes', component: CommandesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'ingredients', component: IngredientsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'menu-semaine', component: MenuSemaineComponent },
+  { path: 'panier', component: PanierComponent },
+  { path: 'plat', component: PlatComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
