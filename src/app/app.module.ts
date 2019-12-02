53bfe6ebbb3a8cc94b4c9ce852056f00ff9de152
MargVander <margauxvanderbeken@gmail.com>
2019-12-03 00:35:40 +0100

    added http module and new ingredient page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -44,7 +45,8 @@ import { ReactiveFormsModule, FormsModule } from "@angular/forms";
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
