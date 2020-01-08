bf9b37507aaa658d1993ede404630987941d5ea8
Ben <ratuz@hotmail.fr>
2020-01-08 13:40:12 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -41,7 +43,9 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu-semaine/menu-semaine.component.ts
+++ src/app/menu-semaine/menu-semaine.component.ts
@@ -1,4 +1,8 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
+
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons, NgbModalOptions} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-menu-semaine',
@@ -7,7 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuSemaineComponent implements OnInit {
 
-  constructor() { }
+  closeResult: string;
+  quantitePlat: number = 1;
+  modalOptions:NgbModalOptions;
+
+    menuForm = this.formBuilder.group({
+    choixPlat: ['', Validators.required],
+    platFormuleChoix: ['', Validators.required],
+    quantiteRepas: [  '', Validators.required]
+    });
+
+  constructor( private modalService: NgbModal, private formBuilder : FormBuilder) {
+    
+      this.modalOptions = {
+      backdrop: 'static',
+      backdropClass: 'customBackdrop',
+      size: 'lg'
+    }
+   }
+
+  openLg(content) {
+    this.modalService.open(content, this.modalOptions).result.then((result) => { //ouvre une fenetre modal
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+      } else {
+        return  `with: ${reason}`;
+      }
+    }
+
+  onSubmit() {
+    console.log('Form submitted !', this.menuForm.value);
+    this.menuForm.reset();
+  }
 
   ngOnInit() {
   }
